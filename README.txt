ea5ddf85c62941780fdcd0c4be58db651e4477e3
YOUWILLDIE666 <youwilldie6.066@gmail.com>
2024-11-18 23:28:30 +0300

    rewrite this too

--- README.txt
+++ README.txt
@@ -1,5 +1,5 @@
-Memory Leak Simulator Simulator
-=========================================================\_
+Memory Leak Your Personal Computer For Free
+===========================================================-
 you probably shouldn't run this if you have prior 16GB RAM |
-==========================================================/
-Copyright (c) 2024, YOUWILLDIE666
+===========================================================-
+Copyright (c) 2024 YOUWILLDIE666
--- main.c
+++ main.c
@@ -1,38 +1,91 @@
-#include "etc/color.h"
+#include <windows.h>
+#include <color.h>
 #include <stdlib.h>
 #include <time.h>
 #include <psapi.h>
+#include <stdio.h>
+#include <limits.h>
 
-static void leak() {
+static void leak()
+{
     int* ptr = (int*)malloc(sizeof(int) * 10000000);
-    for (int j = 0; j < 100; ++j) ptr[j] = rand() % 100;
+    if (ptr == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
+    }
+    for (int j = 0; j < 100; ++j)
+    {
+        ptr[j] = rand() % 100;
+    }
     char css[100];
-    snprintf(css, sizeof(css), "Allocated memory: %p", (void*)ptr);
+    snprintf(css, sizeof(css), "Allocated memory: 0x%p", (void*)ptr);
     char* colored = colorize(css, MAGENTA);
     printf("%s\n", colored);
     free(colored);
 }
 
-static void death() {
-    exit(1);
+static void printc(const char* message, ColorCode color)
+{
+    char* colored = colorize(message, color);
+    printf("%s\n", colored);
+    free(colored);
 }
 
-int main() {
-    int i;
+static double cpu()
+{
+    FILETIME idleTime, kernelTime, userTime;
+    static FILETIME lastIdleTime = {0}, lastKernelTime = {0}, lastUserTime = {0};
+
+    GetSystemTimes(&idleTime, &kernelTime, &userTime);
+
+    ULARGE_INTEGER idle, kernel, user;
+    idle.LowPart = idleTime.dwLowDateTime;
+    idle.HighPart = idleTime.dwHighDateTime;
+    kernel.LowPart = kernelTime.dwLowDateTime;
+    kernel.HighPart = kernelTime.dwHighDateTime;
+    user.LowPart = userTime.dwLowDateTime;
+    user.HighPart = userTime.dwHighDateTime;
+
+    ULARGE_INTEGER lastIdle, lastKernel, lastUser;
+    lastIdle.LowPart = lastIdleTime.dwLowDateTime;
+    lastIdle.HighPart = lastIdleTime.dwHighDateTime;
+    lastKernel.LowPart = lastKernelTime.dwLowDateTime;
+    lastKernel.HighPart = lastKernelTime.dwHighDateTime;
+    lastUser.LowPart = lastUserTime.dwLowDateTime;
+    lastUser.HighPart = lastUserTime.dwHighDateTime;
+
+    ULONGLONG idleDiff = idle.QuadPart - lastIdle.QuadPart;
+    ULONGLONG kernelDiff = kernel.QuadPart - lastKernel.QuadPart;
+    ULONGLONG userDiff = user.QuadPart - lastUser .QuadPart;
+
+    lastIdleTime = idleTime;
+    lastKernelTime = kernelTime;
+    lastUserTime = userTime;
+
+    ULONGLONG total = (kernelDiff + userDiff);
+    if (total > 0)
+    {
+        return (1.0 - (double)idleDiff / total) * 100.0;
+    }
+    return 0.0;
+}
+
+int main()
+{
     srand((unsigned int)time(NULL));
-    printf("Welcome.\n");
     char max_leaks_str[11];
     printf("Enter the maximum number of memory leaks (default: 1000): ");
     fgets(max_leaks_str, sizeof(max_leaks_str), stdin);
 
     int max_leaks;
-    if (sscanf_s(max_leaks_str, "%d", &max_leaks) != 1) {
-        //fprintf(stderr, "Invalid input\n");
-        //exit(1);
+    if (sscanf_s(max_leaks_str, "%d", &max_leaks) != 1)
+    {
         max_leaks = 1000;
     }
 
-    if (max_leaks <= 0 || max_leaks > INT_MAX/* || max_leaks == 1410065407 || max_leaks == 1000100010*/) {
+    if (max_leaks <= 0 || max_leaks > INT_MAX)
+    {
         fprintf(stderr, "You can't do that :3\n");
         exit(1);
     }
@@ -40,30 +93,30 @@ int main() {
     double stability = 100.0;
     double usage = 0.0;
 
-    for (i=0; i < max_leaks; ++i) {
+    for (int i = 0; i < max_leaks; ++i)
+    {
         leak();
         char cs[100];
         snprintf(cs, sizeof(cs), "Memory leaks: %d/%d", i + 1, max_leaks);
-        char* colored1 = colorize(cs, BLUE);
-        printf("%s\n", colored1);
-        free(colored1);
+        printc(cs, BLUE);
 
-        PROCESS_MEMORY_COUNTERS pmc;
-        GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc));
-        usage = (double)pmc.WorkingSetSize / (double)pmc.PagefileUsage * 100.0;
+        usage = cpu();
 
         stability = 100.0 * (1 - (usage / 100.0));
-        if (stability < 0) stability = 0;
+        if (stability < 0)
+        {
+            stability = 0;
+        }
 
         char css[100];
         snprintf(css, sizeof(css), "System stability: %.2f%%", stability);
-        char* colored = colorize(css, RED_BG);
-
-        if ((i+1) % 100 == 0) printf("%s\n", colored);
-        free(colored);
+        if ((i + 1) % 100 == 0)
+        {
+            printc(css, RED_BG);
+        }
     }
 
-    if (i == max_leaks && max_leaks > 0) printf("Congrats! You won.\n"); else death();
+    exit(1);
 
     return 0;
 }
